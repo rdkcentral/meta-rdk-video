44f6e4f6a2a85d1aab52b1f2f6e3389b25bcadda
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-12-01 11:32:10 -0500

    Update bootversion-loader.sh

--- recipes-extended/bootversion-loader/files/bootversion-loader.sh
+++ recipes-extended/bootversion-loader/files/bootversion-loader.sh
@@ -97,7 +97,7 @@ s1_FW_Class=$(grep -m 1 "FW_CLASS" $file_bootversion)
      echo "$s1_FW_Class" >> $file_bootversion
 
 
-MigrationStatus=$(tr181 -g Device.DeviceInfo.Migration.MigrationStatus 2>&1)
+MigrationStatus=$(cat /opt/secure/persistent/MigrationStatus)
 
 boottypeLog "MigrationStatus: $MigrationStatus"
 #comparing slot1 and slot2 FW Class
