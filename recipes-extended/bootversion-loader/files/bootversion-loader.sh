d1cdf3cf642f2a52e35bd6df79c4ea41df629bdd
fzahir786 <FaizalHasan_ZahirHussain@comcast.com>
2026-03-05 19:32:04 +0530

    RDKEMW-14369: Added extra checks

--- recipes-extended/bootversion-loader/files/bootversion-loader.sh
+++ recipes-extended/bootversion-loader/files/bootversion-loader.sh
@@ -41,12 +41,8 @@ if [ -f "$PLATFORM_FILE" ]; then
     boottypeLog "Running the bootversion-loader script for $file_platform devices"
 else
     boottypeLog "Exiting since this script is not intended for this platform"
-    if [ ! -e "$file_bootversion" ]; then
-        echo "BOOT_TYPE=BOOT_INIT" > $file_bootType
-        boottypeLog "BOOT_INIT is set since $file_bootversion is not present"
-    else
-        echo "BOOT_TYPE=BOOT_NORMAL" > $file_bootType
-        boottypeLog "BOOT_NORMAL is set since $file_bootversion is present"
+    echo "BOOT_TYPE=BOOT_NORMAL" > $file_bootType
+    boottypeLog "BOOT_NORMAL is set by default for this platform"
     fi
     exit 0
 fi
@@ -140,7 +136,17 @@ else
     MigrationStatus="NOT_NEEDED"
 fi
 
-boottypeLog "MigrationStatus: $MigrationStatus"
+# verify that Migrationstatus values are one of the expected strings
+case "$MigrationStatus" in
+    "NOT_STARTED"|"NOT_NEEDED"|"STARTED"|"PRIORITY_SETTINGS_MIGRATED"|"DEVICE_SETTINGS_MIGRATED"|"CLOUD_SETTINGS_MIGRATED"|"APP_DATA_MIGRATED"|"MIGRATION_COMPLETED")
+        boottypeLog "Valid MigrationStatus: $MigrationStatus"
+        ;;
+    *)
+        boottypeLog "Invalid MigrationStatus: $MigrationStatus; aborting"
+        exit 1
+        ;;
+esac
+
 #comparing slot1 and slot2 FW Class
 if [ "$v_FW_Class" != "$s1_FW_Class" ]; then
 	# migration fw is run for first time, migration not completed
@@ -150,7 +156,7 @@ if [ "$v_FW_Class" != "$s1_FW_Class" ]; then
 else
      if [ "$MigrationStatus" != "MIGRATION_COMPLETED" ] && [ "$MigrationStatus" != "NOT_NEEDED" ]; then
           echo "BOOT_TYPE=BOOT_MIGRATION" > $file_bootType
-	      boottypeLog "BOOT_MIGRATION since MigrationStatus is not equal to MIGRATION_COMPLETED"
+	      boottypeLog "BOOT_MIGRATION since MigrationStatus is equal to $MigrationStatus"
      elif [ "$MigrationStatus" == "MIGRATION_COMPLETED" ] || [ "$MigrationStatus" == "NOT_NEEDED" ]; then
 	     if [ "$v_version" == "$s1_version" ]; then
 	         echo "BOOT_TYPE=BOOT_NORMAL" > $file_bootType
