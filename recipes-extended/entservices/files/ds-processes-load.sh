2cddbfd7cf7dc21ee365c3a73d88281a52414527
Kirubagaran Jayaraman <kiruba115@gmail.com>
2026-06-04 12:34:25 -0400

    Use pidstat and nethogs for monitoring CPU, memory, IO and network stats

--- recipes-extended/entservices/files/ds-processes-load.sh
+++ recipes-extended/entservices/files/ds-processes-load.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-pidstat -h -u -r -d -p ALL 1 > /opt/logs/ds-processes-load.log
+cleanup() {
+    for pid in "$PIDSTAT_PID" "$NETHOGS_PID"; do
+        if [ -n "$pid" ] && kill -0 "$pid" 2>/dev/null; then
+            kill "$pid" 2>/dev/null
+        fi
+    done
+}
+
+trap cleanup EXIT
+trap 'trap - EXIT; cleanup; exit 130' INT TERM
+
+pidstat -h -u -r -d -p ALL 1 > /opt/logs/ds-processes-load.log &
+PIDSTAT_PID=$!
+
+nethogs -t -d 0.5 >> /opt/logs/ds-processes-nethogs.log &
+NETHOGS_PID=$!
+
+wait
