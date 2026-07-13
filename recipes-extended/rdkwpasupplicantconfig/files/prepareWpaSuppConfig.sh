11a2091dde8bc37803cb235b4a06138fa3398695
jincysam87 <167995204+jincysam87@users.noreply.github.com>
2026-07-13 11:31:24 -0400

    Update prepareWpaSuppConfig.sh

--- recipes-extended/rdkwpasupplicantconfig/files/prepareWpaSuppConfig.sh
+++ recipes-extended/rdkwpasupplicantconfig/files/prepareWpaSuppConfig.sh
@@ -90,8 +90,6 @@ if [ -f $WPA_SUPP_CONF_FILE ]; then
         log "$WPA_SUPP_CONF_FILE file exists and configurations are present"
 		sed -i "/update_config/d" $WPA_SUPP_CONF_FILE
 		sed -i '/network={/,/}/d' $WPA_SUPP_CONF_FILE
-        sed -i "/bssid=/d" $WPA_SUPP_CONF_FILE
-        sed -i "s/key_mgmt=OPEN/key_mgmt=NONE/g" $WPA_SUPP_CONF_FILE
         if grep "country=" "$WPA_SUPP_CONF_FILE"; then
             log "Country code is present , No need to change"
         else
